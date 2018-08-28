7cfbfd73f9549162f0c10273da7f93a0e6a4d7e9
Wali Rahman <wali.rahman89@yahoo.com>
2018-08-28 10:12:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,4 +5,6 @@ function destructivelyAppendDriver (name) {drivers.push(name);}
 
 function destructivelyPrependDriver (name) {drivers.unshift(name);}
 
-function destructivelyRemoveLastDriver () {drivers.pop()}
+function destructivelyRemoveLastDriver () {drivers.pop();}
+
+function destructivelyRemoveFirstDriver () {drivers.shift();}
