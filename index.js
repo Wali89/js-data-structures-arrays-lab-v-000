97fabd95327bf2416f243b935bea724c1868ff1c
Wali Rahman <wali.rahman89@yahoo.com>
2018-08-28 10:11:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,5 @@ const drivers = ["Milo", "Otis", "Garfield"]
 function destructivelyAppendDriver (name) {drivers.push(name);}
 
 function destructivelyPrependDriver (name) {drivers.unshift(name);}
+
+function destructivelyRemoveLastDriver () {drivers.pop}
